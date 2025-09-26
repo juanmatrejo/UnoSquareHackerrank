68c1ab563ac9c96e2f6d3103ebe29f0f25f8d71c
Juan Manuel Trejo <juanma_1996@hotmail.com>
2025-09-26 15:47:09 -0600

    import not used commented

--- src/app/services/rickmorty.service.ts
+++ src/app/services/rickmorty.service.ts
@@ -2,7 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IRickAndMorty } from '../models/rickAndMorty.model';
-import { UrlCodec } from '@angular/common/upgrade';
 
 @Injectable({
   providedIn: 'root'
